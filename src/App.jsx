fbffc9bb849181412aa016296d62941f35569fd5
jbk2 <james@humanbenefits.com>
2025-05-03 13:05:46 +0200

    1st commit, set up with Vite, using Vitest, initial component scaffold done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from './components/navbar'
+import LandingPage from './components/landing-page'
+import './assets/stylesheets/App.css'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <LandingPage />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/navbar.jsx
@@ -0,0 +1,12 @@
+import Cart from './cart'
+
+export default function Navbar() {
+  
+  
+  return(  
+    <div>
+      <h1>LogoType Here</h1>
+      <Cart />
+    </div>
+  )
+}
