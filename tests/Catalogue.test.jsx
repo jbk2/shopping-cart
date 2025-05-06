d0fd644f9f085ad61a6f316293e419dc1e762b70
jbk2 <james@humanbenefits.com>
2025-05-06 15:58:20 +0200

    Further scaffolding
      - Rename Cart Component to CartNavEl
      - Create empty Component and test for CartPage
      - Create empty Component and test for ProductCard
      - Create empty Component and test for Catalogue
      - import product images

---
+++ tests/Catalogue.test.jsx
@@ -0,0 +1,12 @@
+import { describe, it, expect } from 'vitest';
+import { render, screen } from "@testing-library/react";
+import Catalogue from "/src/components/Catalogue.jsx";
+
+describe("Catalogue component", () => {
+  it.skip("", () => {
+    render(<Catalogue />);
+    // const card = screen.getByTestId("card");
+    // expect('').toBeInTheDocument();
+  })
+  
+});
