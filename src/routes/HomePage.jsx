10a21593592190a6394da3ae90901b3ee74d7da9
jbk2 <james@humanbenefits.com>
2025-05-12 09:48:10 +0200

    Adding explicit NavBar.jsx extension on imports while searching for cloudflare build problem

--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -1,5 +1,5 @@
 import LandingContent from "../components/LandingContent"
-import Navbar from "../components/Navbar";
+import Navbar from "../components/Navbar.jsx";
 
 export default function HomePage() {
   return(
